66ac129848ea6049d6f04eedbf35afbd6b94823c
Vincent Lejeune <vincentcmlejeune@gmail.com>
2023-11-26 13:03:07 +0100

    Data fetching in recipe page

--- cooking/src/pages/recipe/Recipe.jsx
+++ cooking/src/pages/recipe/Recipe.jsx
@@ -1,13 +1,27 @@
 import { useEffect, useState } from "react";
 
+import data from "../../data/data.json";
+
 import "./Recipe.css";
 
 export default function Recipe() {
   const [currentRecipe, setCurrentRecipe] = useState(null);
 
   useEffect(() => {
-    const urlRecipe = window.location.pathname.split("/recipe/")[1];
-    setCurrentRecipe(urlRecipe);
+    const foundRecipe = data.recipes.filter(
+      (recipe) =>
+        recipe.lowerCaseName === window.location.pathname.split("/recipe/")[1]
+    );
+    if (foundRecipe.length > 0) {
+      setCurrentRecipe(foundRecipe[0]);
+    } else {
+      setCurrentRecipe("not found");
+    }
   }, []);
-  return <div className="recipe">Current Recipe: {currentRecipe}</div>;
+
+  return currentRecipe !== null && currentRecipe.name ? (
+    <div className="recipe">Current Recipe: {currentRecipe.name}</div>
+  ) : (
+    currentRecipe === "not found" && <div>Not found</div>
+  );
 }
