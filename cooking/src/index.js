62afca5f96cc793f906499e2d073719117c32f83
Vincent Lejeune <vincentcmlejeune@gmail.com>
2023-12-10 12:50:30 +0100

    Placeholder Header

--- cooking/src/index.js
+++ cooking/src/index.js
@@ -1,17 +1,24 @@
+// DEPENDENCIES
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// PAGES
 import App from "./App";
 import Admin from "./pages/admin/Admin";
 import NoMatch from "./pages/no match/NoMatch";
 import Recipe from "./pages/recipe/Recipe";
 
+// COMPONENTS
+import Header from "./components/header/Header";
+
+// STYLING
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Header />
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
