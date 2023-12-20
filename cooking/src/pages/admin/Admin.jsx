b4e71fa58960dcc77edfd54d2dd3cc0e88d0d3fb
Vincent Lejeune <vincentcmlejeune@gmail.com>
2023-12-20 19:27:58 +0100

    Admin pannel update

--- cooking/src/pages/admin/Admin.jsx
+++ cooking/src/pages/admin/Admin.jsx
@@ -1,5 +1,25 @@
 import "./Admin.css";
 
+import { backupData, restoreData } from "../../data/dataHandler";
+
 export default function Admin() {
-  return <div className="admin">Where to add edit and delete</div>;
+  // TO DO ADD POPUPS TO CONFIRM
+  const backupConfirm = () => {
+    backupData();
+  };
+  const restoreConfirm = () => {
+    restoreData();
+  };
+
+  return (
+    <div className="admin">
+      <div>Where to add edit and delete</div>
+      {/* TO DO KEEP TRACK DATE LAST BACKUP */}
+      <div>Last backup:</div>
+      <div>
+        <button onClick={backupConfirm}>Backup Data</button>
+        <button onClick={restoreConfirm}>Restore Data</button>
+      </div>
+    </div>
+  );
 }
