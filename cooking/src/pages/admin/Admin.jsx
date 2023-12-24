00a3eb4cc05a470922f04f095aecdda66d8b3a6b
Vincent Lejeune <vincentcmlejeune@gmail.com>
2023-12-24 09:10:17 +0100

    import fs issue fix

--- cooking/src/pages/admin/Admin.jsx
+++ cooking/src/pages/admin/Admin.jsx
@@ -1,24 +1,17 @@
 import "./Admin.css";
 
-import { backupData, restoreData } from "../../data/dataHandler";
+// import { backupData, restoreData } from "../../data/dataHandler";
+import jsonBackup from "../../data/backupData.json";
+import jsonData from "../../data/data.json";
 
 export default function Admin() {
-  // TO DO ADD POPUPS TO CONFIRM
-  const backupConfirm = () => {
-    backupData();
-  };
-  const restoreConfirm = () => {
-    restoreData();
-  };
-
   return (
     <div className="admin">
       <div>Where to add edit and delete</div>
-      {/* TO DO KEEP TRACK DATE LAST BACKUP */}
-      <div>Last backup:</div>
+      <div>Last backup: {jsonBackup.lastSaved}</div>
       <div>
-        <button onClick={backupConfirm}>Backup Data</button>
-        <button onClick={restoreConfirm}>Restore Data</button>
+        {/* <button onClick={backupConfirm}>Backup Data</button>
+        <button onClick={restoreConfirm}>Restore Data</button> */}
       </div>
     </div>
   );
