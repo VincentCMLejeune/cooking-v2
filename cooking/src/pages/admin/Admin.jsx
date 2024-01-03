ae8d72637badebfeaddafa82779642e093cb6404
Vincent Lejeune <vincentcmlejeune@gmail.com>
2024-01-03 21:15:41 +0100

    Can't do it in client

--- cooking/src/pages/admin/Admin.jsx
+++ cooking/src/pages/admin/Admin.jsx
@@ -1,20 +1,14 @@
 import "./Admin.css";
 
-// import { backupData, restoreData } from "../../data/dataHandler";
 import jsonBackup from "../../data/backupData.json";
-// import jsonData from "../../data/data.json";
+import jsonData from "../../data/data.json";
 
 import { getFullDate } from "./FormatDate";
 
 export default function Admin() {
   return (
     <div className="admin">
-      <div>Where to add edit and delete</div>
       <div>Last backup: {getFullDate(jsonBackup.lastSaved)}</div>
-      <div>
-        {/* <button onClick={backupConfirm}>Backup Data</button>
-        <button onClick={restoreConfirm}>Restore Data</button> */}
-      </div>
     </div>
   );
 }
