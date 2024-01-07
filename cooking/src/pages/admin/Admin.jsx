422468ec9d2c048efb8dcaec7016a893dc8caddd
Vincent Lejeune <vincentcmlejeune@gmail.com>
2024-01-07 10:43:20 +0100

    INtegrated component

--- cooking/src/pages/admin/Admin.jsx
+++ cooking/src/pages/admin/Admin.jsx
@@ -1,5 +1,7 @@
 import "./Admin.css";
 
+import Marmitton from "../../components/marmitton/Marmitton";
+
 import jsonBackup from "../../data/backupData.json";
 // import jsonData from "../../data/data.json";
 
@@ -9,6 +11,7 @@ export default function Admin() {
   return (
     <div className="admin">
       <div>Last backup: {getFullDate(jsonBackup.lastSaved)}</div>
+      <Marmitton />
     </div>
   );
 }
